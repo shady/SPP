fcd91f692466d487ce94c9ab5bdc703093409e1b
shady <shady@shadylog.com>
2009-08-21 01:47:09 +0200

    Cleaned

--- SPP.php
+++ SPP.php
@@ -7,49 +7,35 @@
 
 Class SPP{
 
-		const class_name = 'SPP';
+	const class_name = "[SPP] ";
+	
+	//arguments
+	public $args = array();
+	//output class
+	
+	function SPP($argv)	{
+		Settings::initSettings();
+		$this->args = $argv;
 		
-		//arguments
-		public $args = array();
-		//output class
-		public $output;
-		public $settings;
-		
-		function SPP($argv)	{
-			$this->args = $argv;
-			$output = new Output();
-			//$output->show = false;
-			$settings = new Settings();
-			
-			switch (count($argv)) {
-				case 4:
-					$this->
-					break;
-				case 6:
-					echo "torrentflux";
-					break;
-				default:
-					echo "Not enough arguments, try again later";
-			}
+		switch (count($argv)) {
+			case 7:
+				$this->initJob();
+				break;
+			default:
+				Output::err(self::class_name . "Not enough arguments");
 		}
-		
-		function sabnzbd() {
-		
-		}
-		
-		function torrentflux() {
-		
-		}
+	}
+	
+	function initJob() {
+		//
+		//Make new Job instance.
+		var_dump(Settings::$settings);
+	}
 }
 
 function __autoload($class_name) {
-	require_once 'inc/' . $class_name . '.php';
+	require 'inc/' . $class_name . '.php';
 }
-//Catch terminate.
-
-pcntl_signal(SIGTERM, "cleanUp"); 
 
 $spp = new SPP($argv);
-
-$spp->output->p("Hoi!");
 ?>
